05019fe7f4bc7681c696cd891d34d643d6488105
anders-marten <amarten@ct-group.com>
2026-04-24 17:42:18 +0200

    Update home screen title again

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>No it did not</Text>
+      <Text style={styles.title}>One last test</Text>
       <Text style={styles.subtitle}>by ungap</Text>
       <StatusBar style="dark" />
     </View>
